3ba6218ae69fa7781dad456b28a4f7a476a24587
Nathan Matias <natematias@gmail.com>
2014-02-21 13:27:07 -0500

    added a service for all data associated with something. Changed the json return convention to be nested

--- routes/services.js
+++ routes/services.js
@@ -6,6 +6,12 @@ function url_service(url){
   return {url:url}
 }
 
+function herdict_service(url){
+  var rval = {};
+  rval["url"] = url;
+  return {"herdict": rval}
+}
+
 // validation method
 function validate_url(url){
   if(url!= null && url!=""){
@@ -22,6 +28,23 @@ exports.herdict = function (req, res) {
     if(validate_url(url)){
       // merge the results of the url service into the results values
       _.extend(rval, url_service(url)); 
+      _.extend(rval, herdict_service(url)); 
     }
     res.json(rval);
 };
+
+
+exports.all = function (req, res) {
+    url = req.url.substring(req.url.indexOf('?')+1,req.url.length)
+    rval = {};
+
+    // query string validation
+    if(validate_url(url)){
+      // merge the results of the url service into the results values
+      _.extend(rval, url_service(url)); 
+      _.extend(rval, herdict_service(url)); 
+    }
+    res.json(rval);
+};
+
+
--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.configure('development', function(){
 
 app.get('/', index.view);
 app.get('/herdict', services.herdict);
+app.get('/all', services.all);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
